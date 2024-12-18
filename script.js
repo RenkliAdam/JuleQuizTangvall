02d2a7f0e0c83f0425d003ef80a618f4567bc91e
RenkliAdam <10muhammed@ikrs.no>
2024-12-18 09:40:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-body {
-  font-family: 'Arial', sans-serif;
-  margin: 0;
-  padding: 0;
-  color: #fff;
-  background: linear-gradient(to bottom right, #ff0000, #008000, #ffffff); /* Kırmızı, Yeşil ve Beyaz gradyan */
-  text-align: center;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+function restartGame() {
+  currentQuestion = 0;
+  score = 0;
+  scoreElement.textContent = "";
+  loadQuestion();
 }
-#restart-btn {
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  padding: 10px 20px;
-  font-size: 16px;
-  background-color: #28a745;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background 0.3s;
-}
-#restart-btn:hover {
-  background-color: #218838;
-}
+
